7b7569e28efadc1155ce74b4dcee35ba547a45ae
solvanny <ion.soltan@hotmail.com>
2019-03-20 13:07:25 -0400

    sass implimintation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import logoWhite from "./img/logo-white.png";
 
 import "./App.scss";
 
@@ -6,7 +7,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="header">some text...</header>
+        <header className="header">
+          <div className="header__logo-box">
+            <img src={logoWhite} alt="Logo" className="header__logo" />
+          </div>
+          <div className="header__text-box">
+            <h1 className="heading-primary">
+              <span className="heading-primary--main">Outdoors</span>
+              <span className="heading-primary--sub">
+                is where life happens
+              </span>
+            </h1>
+
+            <a href="#" className="btn btn--white btn--animated">
+              Disover our tours
+            </a>
+          </div>
+        </header>
       </div>
     );
   }
